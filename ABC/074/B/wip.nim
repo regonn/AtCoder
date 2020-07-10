e658b4a17b145104dcb36038e413fa496e8630b1
regonn <programmer@haizine.art>
2020-07-10 11:46:06 +0900

    wip ABC074B

---
+++ ABC/074/B/wip.nim
@@ -0,0 +1,6 @@
+import strutils, sequtils
+let get = iterator: string {.closure.} =
+  for s in stdin.readAll.split: yield s
+proc read: int = get().parseInt
+template rep(p: untyped, n: int): seq = newSeqWith(n, p)
+
