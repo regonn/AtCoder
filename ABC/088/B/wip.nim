111d76c3bc226f07b4ded4eafe534523475a4979
regonn <programmer@haizine.art>
2020-07-12 09:36:42 +0900

    wip ABC088B

---
+++ ABC/088/B/wip.nim
@@ -0,0 +1,24 @@
+import strutils, sequtils, lists
+let get = iterator: string {.closure.} =
+  for s in stdin.readAll.split: yield s
+proc read: int = get().parseInt
+template rep(p: untyped, n: int): seq = newSeqWith(n, p)
+
+let
+  n = read()
+
+
+var
+  a : SinglyLinkedRing[int] = initSinglyLinkedRing[int]()
+  alice: seq[int]
+  bob: seq[int]
+
+for _ in 0..(n-1):
+  a.append(read())
+
+echo a
+
+# while len(a) > 0:
+#   let max_value = max(a)
+#   a.remove(a.find(max_value))
+#   if len(alice) == len(bob):
