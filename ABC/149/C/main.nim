075eb4827d747ab871fdafbc60c4fdf99a4e9b3e
regonn <programmer@haizine.art>
2020-07-27 11:20:45 +0900

    ABC149C

---
+++ ABC/149/C/main.nim
@@ -0,0 +1,21 @@
+import strutils, sequtils
+let get = iterator: string {.closure.} =
+  for s in stdin.readAll.split: yield s
+proc read: int = get().parseInt
+
+proc is_prime(number: int): bool =
+    if ( number <= 1 ):
+        return false
+    var index = 2
+    while index * index <= number:
+        if ( number mod index == 0 ):
+            return false
+        index += 1
+    return true
+
+var number = read()
+
+while not is_prime(number):
+    number += 1
+
+echo number
